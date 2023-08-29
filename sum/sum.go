920f988701f9c7acd174ccdc00fcdb71f68d0380
wittakarn keeratichayakorn <wittakarn.expert@gmail.com>
2023-08-29 23:39:36 +0700

    Finish array and slices lesson

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,28 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := []int{}
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := []int{}
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) > 0 {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,18 +1,11 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		actual := Sum(numbers)
-		expected := 15
-
-		if actual != expected {
-			t.Errorf("got %d want %d given, %v", actual, expected, numbers)
-		}
-	})
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		actual := Sum(numbers)
@@ -22,4 +15,45 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", actual, expected, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		actual := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
+
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		actual := SumAll()
+		expected := []int{}
+
+		checkSums(t, actual, expected)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		actual := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+
+		checkSums(t, actual, expected)
+	})
+
+}
+
+func checkSums(t testing.TB, actual, expected []int) {
+	t.Helper()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("actual %v expected %v", actual, expected)
+	}
 }
